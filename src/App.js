89d508838a07952d0ab3788181f5f98403d1da45
Ian Cahn <iancahn@live.com>
2022-07-31 20:59:13 +0200

    Fixed components error
    I added components to public folder.... I am really good at this.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React from 'react';
+// import Components
+import Nav from './components/Nav';
 // import Styling
 import './styles/styles.scss'
+
+
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Hello</h1>
     </div>
   );
